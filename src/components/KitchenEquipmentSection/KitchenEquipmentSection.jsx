5a91b45c247b9d8b9ef4967eceb673d38cb46ca7
Ne0sky <mailmeakash2@gmail.com>
2023-08-20 21:23:17 +0530

    add ingredients section component, kitchen equipment section component

---
+++ src/components/KitchenEquipmentSection/KitchenEquipmentSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import KitchenEquipmentCard from '../KitchenEquipmentCard'
+import KitchenEquipmentData from '../../Data/KitchenEquipmentData'
+const KitchenEquipmentSection = () => {
+  return (
+    <div className="bg-zinc-800 text-white flex flex-col justify-center items-center  py-16">
+      {/* SEASONAL DISHES */}
+      <h1 className='text-3xl font-semibold pb-8'>Kitchen Equipments</h1>
+      <div className="card-container grid grid-cols-2  md:grid-cols-2 lg:grid-cols-4  gap-4 lg:gap-16 items-center justify-center">
+      {KitchenEquipmentData.map((item, index) => (
+        <KitchenEquipmentCard key={index} title={item.title}  image={item.image}  />
+      ))}
+      </div>
+
+    </div>
+  )
+}
+
+export default KitchenEquipmentSection
